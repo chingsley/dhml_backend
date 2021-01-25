9a50e7a7366a894f97a5f99de556888ad05e0dbf
chingsley <eneja.kc@gmail.com>
2021-01-25 12:35:13 +0100

    generate manifest

--- src/modules/hcp/hcp.services.js
+++ src/modules/hcp/hcp.services.js
@@ -1,5 +1,6 @@
 import db from '../../database/models';
 import AppService from '../app/app.service';
+import { getManifest } from '../../database/scripts/hcp.scripts';
 
 export default class HcpService extends AppService {
   constructor({ body, files, query, params }) {
@@ -20,20 +21,29 @@ export default class HcpService extends AppService {
     });
   }
   async fetchManifest() {
-    return await db.HealthCareProvider.findAndCountAll({
-      where: { ...this.filterHcp() },
-      include: {
-        model: db.Enrollee,
-        as: 'enrollees',
-        where: { principalId: null },
-        include: { model: db.Enrollee, as: 'dependants' },
-      },
-      ...this.paginate(),
-    });
+    const { page, pageSize } = this.query;
+    let count, rows;
+    if (page && pageSize) {
+      count = (await this.executeQuery(getManifest)).length;
+      rows = await this.executeQuery(getManifest, this.query);
+    } else {
+      rows = await this.executeQuery(getManifest, this.query);
+      count = rows.length;
+    }
+    return { count, rows };
+    // return await db.HealthCareProvider.findAndCountAll({
+    //   where: { ...this.filterHcp() },
+    //   include: {
+    //     model: db.Enrollee,
+    //     as: 'enrollees',
+    //     where: { principalId: null },
+    //     include: { model: db.Enrollee, as: 'dependants' },
+    //   },
+    //   ...this.paginate(),
+    // });
   }
   async filterHcp() {
     return this.filterBy(['hcpCode', 'hcpName'], {
-      modelName: 'HealthCareProvider',
       map: {
         hcpCode: 'code',
         hcpName: 'name',
