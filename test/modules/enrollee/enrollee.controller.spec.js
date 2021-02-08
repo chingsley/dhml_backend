ab404a7e9de4f434abcd465d9a5ea4ad1f7d8962
chingsley <eneja.kc@gmail.com>
2021-02-08 04:51:21 +0100

    test feature to delete an enrollee

--- test/modules/enrollee/enrollee.controller.spec.js
+++ test/modules/enrollee/enrollee.controller.spec.js
@@ -570,4 +570,93 @@ describe('EnrolleeController', () => {
       }
     });
   });
+  describe('deleteEnrollee', () => {
+    let sampleEnrollees, token, hcp, samplePrncpl1, samplePrncpl2;
+    let principal1, principal2;
+
+    beforeAll(async () => {
+      await TestService.resetDB();
+      const HCPs = await TestService.seedHCPs(1);
+      hcp = HCPs[0];
+
+      sampleEnrollees = getEnrollees({
+        numOfPrincipals: 2,
+        sameSchemeDepPerPrincipal: 1,
+        vcshipDepPerPrincipal: 1,
+      });
+      const principals = TestService.removeNullValues(
+        sampleEnrollees.principals
+      );
+      samplePrncpl1 = {
+        ...principals[0],
+        scheme: 'AFRSHIP',
+        hcpId: hcp.id,
+        staffNumber: null,
+        serviceNumber: 'NN/0002',
+        enrolleeIdNo: '000231',
+      };
+      samplePrncpl2 = {
+        ...principals[1],
+        scheme: 'AFRSHIP',
+        hcpId: hcp.id,
+        staffNumber: null,
+        serviceNumber: 'NN/0003',
+        enrolleeIdNo: '000232',
+      };
+      const [p1, p2] = await TestService.seedEnrollees([
+        samplePrncpl1,
+        samplePrncpl2,
+      ]);
+      principal1 = p1;
+      principal2 = p2;
+      await EnrolleeTest.addDependantsToPrincipal(
+        sampleEnrollees.dependants.map((depdnt, i) => ({
+          ...depdnt,
+          enrolleeIdNo: `${p2.enrolleeIdNo}-${i + 1}`,
+        })),
+        p2
+      );
+      const { sampleStaffs } = getSampleStaffs(1);
+      const data = await TestService.getToken(
+        sampleStaffs[0],
+        ROLES.SUPERADMIN
+      );
+      token = data.token;
+    });
+
+    it('it returns status 200 and a success message', async (done) => {
+      try {
+        const res = await EnrolleeTest.deleteEnrollee(principal1.id, token);
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('message');
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+    it('it deletes all of a principal"s dependant along with the principal', async (done) => {
+      try {
+        await EnrolleeTest.deleteEnrollee(principal2.id, token);
+        const depedants = await EnrolleeTest.getPrincipalDependants(
+          principal2.id
+        );
+        expect(depedants).toHaveLength(0);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+    it('returns not found error if the enrolleeId is not founc', async (done) => {
+      try {
+        const nonExistingId = principal2.id * 5;
+        const res = await EnrolleeTest.deleteEnrollee(nonExistingId, token);
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('errors');
+        expect(res.body.errors[0]).toMatch(/no record found for/i);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
 });
--- test/modules/enrollee/enrollee.controllers/updateEnrollee.spec.js
+++ test/modules/enrollee/enrollee.controllers/updateEnrollee.spec.js
@@ -7,9 +7,9 @@ import EnrolleeTest from '../enrollee.test.service';
 // const { log } = console;
 
 describe('EnrolleeController', () => {
-  describe('unverifyEnrollee', () => {
-    let sampleEnrollees, token, hcp, samplePrincipal;
-    let principal, res, unverifiedPrincipal;
+  describe('deleteEnrollee', () => {
+    let sampleEnrollees, token, hcp, samplePrncpl1, samplePrncpl2;
+    let principal1, principal2;
 
     beforeAll(async () => {
       await TestService.resetDB();
@@ -17,29 +17,41 @@ describe('EnrolleeController', () => {
       hcp = HCPs[0];
 
       sampleEnrollees = getEnrollees({
-        numOfPrincipals: 1,
+        numOfPrincipals: 2,
         sameSchemeDepPerPrincipal: 1,
         vcshipDepPerPrincipal: 1,
       });
       const principals = TestService.removeNullValues(
         sampleEnrollees.principals
       );
-      samplePrincipal = {
+      samplePrncpl1 = {
         ...principals[0],
         scheme: 'AFRSHIP',
         hcpId: hcp.id,
         staffNumber: null,
         serviceNumber: 'NN/0002',
         enrolleeIdNo: '000231',
-        isVerified: true,
       };
-      const [seededPrincipal] = await TestService.seedEnrollees([
-        samplePrincipal,
+      samplePrncpl2 = {
+        ...principals[1],
+        scheme: 'AFRSHIP',
+        hcpId: hcp.id,
+        staffNumber: null,
+        serviceNumber: 'NN/0003',
+        enrolleeIdNo: '000232',
+      };
+      const [p1, p2] = await TestService.seedEnrollees([
+        samplePrncpl1,
+        samplePrncpl2,
       ]);
-      principal = seededPrincipal;
+      principal1 = p1;
+      principal2 = p2;
       await EnrolleeTest.addDependantsToPrincipal(
-        sampleEnrollees.dependants.map((d) => ({ ...d, isVerified: true })),
-        principal
+        sampleEnrollees.dependants.map((depdnt, i) => ({
+          ...depdnt,
+          enrolleeIdNo: `${p2.enrolleeIdNo}-${i + 1}`,
+        })),
+        p2
       );
       const { sampleStaffs } = getSampleStaffs(1);
       const data = await TestService.getToken(
@@ -47,12 +59,11 @@ describe('EnrolleeController', () => {
         ROLES.SUPERADMIN
       );
       token = data.token;
-      res = await EnrolleeTest.unverifyPrincipal(principal.id, token);
-      unverifiedPrincipal = await EnrolleeTest.findById(principal.id);
     });
 
     it('it returns status 200 and a success message', async (done) => {
       try {
+        const res = await EnrolleeTest.deleteEnrollee(principal1.id, token);
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty('message');
         done();
@@ -60,24 +71,25 @@ describe('EnrolleeController', () => {
         done(e);
       }
     });
-    it('it sets the enrollee"s isVerified status to false and dateVerified to null', async (done) => {
+    it('it deletes all of a principal"s dependant along with the principal', async (done) => {
       try {
-        expect(unverifiedPrincipal.isVerified).toBe(false);
-        expect(unverifiedPrincipal.dateVerified).toBe(null);
+        await EnrolleeTest.deleteEnrollee(principal2.id, token);
+        const depedants = await EnrolleeTest.getPrincipalDependants(
+          principal2.id
+        );
+        expect(depedants).toHaveLength(0);
         done();
       } catch (e) {
         done(e);
       }
     });
-    it('it also unverifies all dependants under the principal', async (done) => {
+    it('returns not found error if the enrolleeId is not founc', async (done) => {
       try {
-        for (let {
-          isVerified,
-          dateVerified,
-        } of unverifiedPrincipal.dependants) {
-          expect(isVerified).toBe(false);
-          expect(dateVerified).toBe(null);
-        }
+        const nonExistingId = principal2.id * 5;
+        const res = await EnrolleeTest.deleteEnrollee(nonExistingId, token);
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty('errors');
+        expect(res.body.errors[0]).toMatch(/no record found for/i);
         done();
       } catch (e) {
         done(e);
