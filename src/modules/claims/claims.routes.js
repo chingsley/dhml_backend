1a1ec1769f7f9587305d5bfd0ac12874cfdab300
chingsley <eneja.kc@gmail.com>
2021-07-10 23:16:27 +0100

    - Remove the endpoint to get single claim by id

--- src/modules/claims/claims.routes.js
+++ src/modules/claims/claims.routes.js
@@ -22,12 +22,6 @@ router.get(
   ClaimsMiddleware.validateClaimsSearchQuery,
   ClaimsController.getClaims
 );
-router.get(
-  '/:claimId',
-  AuthMiddleware.authorize([MD, HCP, TIER_1_MEDICAL, TIER_2_MEDICAL]),
-  AppMiddleware.validateIdParams,
-  ClaimsController.getSingleClaimById
-);
 
 router.patch(
   '/',
