e192682b7a90cee55a1600a256bcc321a22cb604
chingsley <eneja.kc@gmail.com>
2021-09-25 09:17:25 +0100

    - Update password reset function

--- src/utils/Sendgrid/index.js
+++ src/utils/Sendgrid/index.js
@@ -28,28 +28,4 @@ export default class Sendgrid {
       return error;
     }
   }
-
-  static getPasswordResetTemplate(resetToken) {
-    const msg = `
-  
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <style>
-        body {background-color: red; padding: 5px;}
-        div {color: white; padding: 15px; padding-left: 10px; width: 80%; border-radius: 5px;}
-        h1   {color: dark red;}
-      </style>
-    </head>
-    <body>
-      <div style="background-color: #130f40;">
-          <p>click the link below to reset your password</p>
-          <a href="${process.env.HOST_URL_FRONTEND}/password/reset/${resetToken}">${process.env.HOST_URL_FRONTEND}/password/reset/${resetToken}</a>
-        <div />
-    </body>
-  </html>
-  `;
-
-    return msg;
-  }
 }
